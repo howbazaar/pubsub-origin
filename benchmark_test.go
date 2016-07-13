5636856d22065724b0bbb3445889e50bcae47a5f
John Arbash Meinel <john@arbash-meinel.com>
2016-07-13 16:38:27 +0400

    sad-face. On Windows we need 5ms instead of just 1ms to reliably not fail the benchmark.

--- benchmark_test.go
+++ benchmark_test.go
@@ -56,3 +56,36 @@ func (*BenchmarkSuite) BenchmarkPublishAndWaitOneSubscriber(c *gc.C) {
 	c.Check(failedCount, gc.Equals, 0)
 	c.Check(counter, gc.Equals, c.N)
 }
+
+func (*BenchmarkSuite) BenchmarkPublishAndWaitManySubscribers(c *gc.C) {
+	hub := pubsub.NewSimpleHub()
+	topic := pubsub.Topic("benchmarking")
+	const numSubs = 100
+	counters := make([]int, numSubs)
+	for i := 0; i < numSubs; i++ {
+		i := i
+		hub.Subscribe(topic, func(topic pubsub.Topic, data interface{}) {
+			counters[i]++
+		})
+	}
+	failedCount := 0
+	for i := 0; i < c.N; i++ {
+		result, err := hub.Publish(topic, nil)
+		c.Assert(err, jc.ErrorIsNil)
+
+		select {
+		case <-result.Complete():
+		case <-time.After(5 * veryShortTime):
+			failedCount++
+		}
+	}
+	// XXX: on my VM, this fails about half the time.
+	c.Check(failedCount, gc.Equals, 0)
+	total := 0
+	for i := 0; i < numSubs; i++ {
+		c.Check(counters[i], gc.Equals, c.N,
+			gc.Commentf("wrong counter amount %d != %d for %d", counters[i], c.N, i))
+		total += counters[i]
+	}
+	c.Check(total, gc.Equals, c.N*numSubs)
+}
