234394ef2b737a9c56cfc2f3b542ce55ca8f46c5
John Arbash Meinel <john@arbash-meinel.com>
2016-07-14 16:58:10 +0400

    Check errors from everything, and make it easier to affect how many subscribers we test.

--- benchmark_test.go
+++ benchmark_test.go
@@ -12,6 +12,8 @@ import (
 	"github.com/juju/pubsub"
 )
 
+const numSubscribers = 100
+
 var _ = gc.Suite(&BenchmarkSuite{})
 
 type BenchmarkSuite struct{}
@@ -38,9 +40,11 @@ func (*BenchmarkSuite) BenchmarkSimplePublishAndWaitOneSubscriber(c *gc.C) {
 	hub := pubsub.NewSimpleHub()
 	topic := pubsub.Topic("benchmarking")
 	counter := 0
-	hub.Subscribe(topic, func(topic pubsub.Topic, data interface{}) {
+	sub, err := hub.Subscribe(topic, func(topic pubsub.Topic, data interface{}) {
 		counter += 1
 	})
+	defer sub.Unsubscribe()
+	c.Assert(err, jc.ErrorIsNil)
 	failedCount := 0
 	for i := 0; i < c.N; i++ {
 		result, err := hub.Publish(topic, nil)
@@ -60,13 +64,14 @@ func (*BenchmarkSuite) BenchmarkSimplePublishAndWaitOneSubscriber(c *gc.C) {
 func (*BenchmarkSuite) BenchmarkSimplePublishAndWaitManySubscribers(c *gc.C) {
 	hub := pubsub.NewSimpleHub()
 	topic := pubsub.Topic("benchmarking")
-	const numSubs = 100
-	counters := make([]int, numSubs)
-	for i := 0; i < numSubs; i++ {
+	counters := make([]int, numSubscribers)
+	for i := 0; i < numSubscribers; i++ {
 		i := i
-		hub.Subscribe(topic, func(topic pubsub.Topic, data interface{}) {
+		sub, err := hub.Subscribe(topic, func(topic pubsub.Topic, data interface{}) {
 			counters[i]++
 		})
+		c.Assert(err, jc.ErrorIsNil)
+		defer sub.Unsubscribe()
 	}
 	failedCount := 0
 	for i := 0; i < c.N; i++ {
@@ -79,15 +84,85 @@ func (*BenchmarkSuite) BenchmarkSimplePublishAndWaitManySubscribers(c *gc.C) {
 			failedCount++
 		}
 	}
-	// XXX: on my VM, this fails about half the time.
 	c.Check(failedCount, gc.Equals, 0)
 	total := 0
-	for i := 0; i < numSubs; i++ {
-		c.Check(counters[i], gc.Equals, c.N,
-			gc.Commentf("wrong counter amount %d != %d for %d", counters[i], c.N, i))
+	expected := make([]int, numSubscribers)
+	for i := 0; i < numSubscribers; i++ {
+		expected[i] = c.N
 		total += counters[i]
 	}
-	c.Check(total, gc.Equals, c.N*numSubs)
+	c.Check(total, gc.Equals, c.N*numSubscribers)
+}
+
+func (*BenchmarkSuite) BenchmarkStructuredPublishAndWaitOneSubscriber(c *gc.C) {
+	source := Emitter{
+		Origin:  "test",
+		Message: "hello world",
+		ID:      42,
+	}
+	hub := pubsub.NewStructuredHub(nil)
+	topic := pubsub.Topic("benchmarking")
+	counter := 0
+	sub, err := hub.Subscribe(topic, func(topic pubsub.Topic, s Emitter, err error) {
+		counter++
+	})
+	c.Assert(err, jc.ErrorIsNil)
+	defer sub.Unsubscribe()
+	failedCount := 0
+	for i := 0; i < c.N; i++ {
+		// Publishing nil results in a nil-pointer dereference
+		result, err := hub.Publish(topic, source)
+		c.Assert(err, jc.ErrorIsNil)
+
+		select {
+		case <-result.Complete():
+		case <-time.After(5 * veryShortTime):
+			failedCount++
+		}
+	}
+	c.Check(failedCount, gc.Equals, 0)
+	c.Check(counter, gc.Equals, c.N)
+}
+
+func (*BenchmarkSuite) BenchmarkStructuredPublishAndWaitManySubscribers(c *gc.C) {
+	source := Emitter{
+		Origin:  "test",
+		Message: "hello world",
+		ID:      42,
+	}
+	hub := pubsub.NewStructuredHub(nil)
+	topic := pubsub.Topic("benchmarking")
+	counters := make([]int, numSubscribers)
+	for i := 0; i < numSubscribers; i++ {
+		mycounter := i
+		sub, err := hub.Subscribe(topic, func(topic pubsub.Topic, s Emitter, err error) {
+			counters[mycounter]++
+		})
+		c.Assert(err, jc.ErrorIsNil)
+		defer sub.Unsubscribe()
+	}
+	failedCount := 0
+	for i := 0; i < c.N; i++ {
+		// Publishing nil results in a nil-pointer dereference
+		result, err := hub.Publish(topic, source)
+		c.Assert(err, jc.ErrorIsNil)
+
+		select {
+		case <-result.Complete():
+		case <-time.After(5 * veryShortTime):
+			failedCount++
+		}
+	}
+	// XXX: on my machine, this fails about half the time if I use the same 1ms
+	c.Check(failedCount, gc.Equals, 0)
+	total := 0
+	expected := make([]int, numSubscribers)
+	for i := 0; i < numSubscribers; i++ {
+		expected[i] = c.N
+		total += counters[i]
+	}
+	c.Check(counters, gc.DeepEquals, expected)
+	c.Check(total, gc.Equals, c.N*numSubscribers)
 }
 
 func (*BenchmarkSuite) BenchmarkPublishAndWaitMultiplexedSubscribers(c *gc.C) {
@@ -101,9 +176,8 @@ func (*BenchmarkSuite) BenchmarkPublishAndWaitMultiplexedSubscribers(c *gc.C) {
 	c.Assert(err, jc.ErrorIsNil)
 	defer sub.Unsubscribe()
 	topic := pubsub.Topic("benchmarking")
-	const numSubs = 10
-	counters := make([]int, numSubs)
-	for i := 0; i < numSubs; i++ {
+	counters := make([]int, numSubscribers)
+	for i := 0; i < numSubscribers; i++ {
 		mycounter := i
 		err := multi.Add(pubsub.MatchAll, func(topic pubsub.Topic, s Emitter, err error) {
 			counters[mycounter]++
@@ -125,10 +199,10 @@ func (*BenchmarkSuite) BenchmarkPublishAndWaitMultiplexedSubscribers(c *gc.C) {
 	// XXX: on my machine, this fails about half the time if I use the same 1ms
 	c.Check(failedCount, gc.Equals, 0)
 	total := 0
-	for i := 0; i < numSubs; i++ {
+	for i := 0; i < numSubscribers; i++ {
 		c.Check(counters[i], gc.Equals, c.N,
 			gc.Commentf("wrong counter amount %d != %d for %d", counters[i], c.N, i))
 		total += counters[i]
 	}
-	c.Check(total, gc.Equals, c.N*numSubs)
+	c.Check(total, gc.Equals, c.N*numSubscribers)
 }
